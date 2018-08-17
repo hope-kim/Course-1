c20785f6f7fc17c0b2f5a805ff22780527abc213
Hope Kim <hopekim@usc.edu>
2018-08-17 01:45:23 -0700

    Add files via upload

---
+++ lp01.cpp
@@ -0,0 +1,27 @@
+// Hope Kim
+// ITP 165, Fall 2017
+// Lab Practical 1
+// hopekim@usc.edu
+
+#include <iostream>
+#include <string>
+int main()
+{
+    std::string animal;
+    std::string verb;
+    int whole = 0;
+    
+    std::cout << "Please give me an animal (in plural form): ";
+    std::cin >> animal;
+    std::cout << "Please give me a verb ending in -ing: ";
+    std::cin >> verb;
+    std::cout << "Please give me a whole number: ";
+    std::cin >> whole;
+    
+    int total = whole*2;
+    
+    std::cout << "The " << whole << " pairs of " << animal << " go " << verb << "." << std::endl;
+    std::cout << "That means there was a total of " << total << " " << animal << "." << std::endl;
+    
+    return 0;
+}
