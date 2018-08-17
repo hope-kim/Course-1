676aa50199bda7a6bea581150180e39cd3f1c3b2
Hope Kim <hopekim@usc.edu>
2018-08-17 01:51:44 -0700

    Update lp01.cpp

--- lp01.cpp
+++ lp01.cpp
@@ -7,10 +7,12 @@
 #include <string>
 int main()
 {
+    // variables to hold user input
     std::string animal;
     std::string verb;
     int whole = 0;
     
+    // asking for user input
     std::cout << "Please give me an animal (in plural form): ";
     std::cin >> animal;
     std::cout << "Please give me a verb ending in -ing: ";
@@ -18,8 +20,10 @@ int main()
     std::cout << "Please give me a whole number: ";
     std::cin >> whole;
     
+    // calculations
     int total = whole*2;
     
+    // output
     std::cout << "The " << whole << " pairs of " << animal << " go " << verb << "." << std::endl;
     std::cout << "That means there was a total of " << total << " " << animal << "." << std::endl;
     
