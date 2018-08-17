fcbb38a8f30a7f579601103d64acafa4b544c360
Hope Kim <hopekim@usc.edu>
2018-08-17 12:58:16 -0700

    Update and rename lp18.cpp to lp16/lp18.cpp

--- lp16/lp18.cpp
+++ lp16/lp18.cpp
@@ -1,6 +1,6 @@
 // Name
 // ITP 165, Fall 2017
-// Lab Practical 18
+// Lab Practical 16
 // hopekim@usc.edu
 
 #include "StrLib.h"
@@ -9,14 +9,18 @@
 
 int main()
 {
+    // user input
     std::string input;
     std::vector<std::string> words;
     double sum = 0;
     double average = 0.0;
     std::cout << "Please enter a list of words separated by commas: ";
     std::cin >> input;
+    
+    // splits line through commas
     words = strSplit(input, ',');
     
+    // loops through all words
     for (int i = 0; i < words.size(); i++)
     {
         sum += (words[i]).size();
